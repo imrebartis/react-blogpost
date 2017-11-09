6bbdc4ccb1409de264ed1bbd9ae4a9fec037d4a7
Imre Bartis <imre.bartis@siili.com>
2017-11-09 15:32:24 +0200

    receiving new posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,19 @@
 import _ from "lodash";
-import { FETCH_POSTS } from "../actions";
+import { FETCH_POSTS, FETCH_POST } from "../actions";
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+    // const post = action.payload.data;
+    // const newState = { ...state };
+    // newState[post.id] = post;
+    // return newState;
+
+    // [action.payload.data.id] is key interpolation
+    // (= whatever action.payload.data.id is, make
+    // a new key on this value + set
+    // its value to action.payload.data )
+    return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
     // turning the array of posts (see http://reduxblog.herokuapp.com/)
     // into an object where the keys are the posts id:
