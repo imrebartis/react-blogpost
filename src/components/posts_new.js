ee1e7aa1bb863c26a1c9a08aa734d4a7688cc9a2
Imre Bartis <imre.bartis@siili.com>
2017-11-09 12:00:37 +0200

    field components added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-    
+    renderField(field) {
+        return (
+          <div>
+            <label>{field.label}</label>
+            <input {...field.input} />
+          </div>
+        );
+      }
+
     render() {
         return (
             <form>
                  <Field
+                    label="Title For Post"
                     name="title"
-                    component={}
+                    component={this.renderField}
                  />
+                  <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                    />
+                  <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                  />
             </form>
         );
     }
