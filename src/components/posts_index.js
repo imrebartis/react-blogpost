f6ff09013cfe6b263d370ca70e23953b70a85312
Imre Bartis <imre.bartis@siili.com>
2017-11-09 11:01:27 +0200

    list of posts is rendered

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,14 +9,31 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+            {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    console.log(this.props.posts);
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
 
-export default connect (null, { fetchPosts })(PostsIndex);
+export default connect (mapStateToProps, { fetchPosts })(PostsIndex);
