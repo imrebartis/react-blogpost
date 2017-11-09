c0ff848b53c40868ba2385b731bb0efd5d2dac1a
Imre Bartis <imre.bartis@siili.com>
2017-11-09 16:17:41 +0200

    caching records

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
@@ -14,9 +15,10 @@ class PostsShow extends Component {
         if (!post) {
             return <div>Loading...</div>;
           }
-          
+
         return (
             <div>
+                <Link to="/">Back To Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
